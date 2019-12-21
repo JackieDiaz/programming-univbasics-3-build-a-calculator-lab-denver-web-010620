0c2d8944e34ef3d71100b958a2f224e979d253cd
alexander diaz <alexanderjdiaz89@gmail.com>
2019-12-21 02:46:55 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,17 +1,21 @@
 def addition(num1, num2)
-
+sum = a + b
+sum
 end
 
 def subtraction(num1, num2)
-
+subtract = a - b 
+subtract
 end
 
 def division(num1, num2)
-
+divide = a / b 
+divide
 end
 
 def multiplication(num1, num2)
-
+times = a * b
+times
 end
 
 def modulo(num1, num2)
